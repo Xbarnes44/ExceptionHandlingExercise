087406b6111c51f2867da443b0d471da439eb9c8
Xavier Barnes <xdbcoder44@gmail.com>
2022-10-06 23:23:14 -0400

    Finished Exercise

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 
 namespace ExceptionHandlingExercise
@@ -8,12 +9,27 @@ namespace ExceptionHandlingExercise
     {
         static void Main(string[] args)
         {
+
+            var x = 1;
+
+            for(;x< 6; x += 2)
+            {
+                x = x *x;
+            }
+            Console.WriteLine(x);
             // -----------------------------------------------------------------------------
             // First create an char[], it must contain 6 numbers and 3 letters - name it arr
+
+            var arr = new char[] { '1', '2', '3', '4', '5', '6', 'a', 'b', 'c' };
             // Create a list called numbers that will hold integers
+
+            var numbers = new List<int>();
             // Create an string variable with an empty string initializer - name it str
 
+            string str = "";
+
             // using a foreach loop, attempt to parse the elements in your char[] with int.Parse()
+
             // and Exceptions will be thrown 
             // Below we will set this up 
             // ------------------------------------------------------------------------------
@@ -23,9 +39,21 @@ namespace ExceptionHandlingExercise
             //TODO START HERE:
             
             // Make a foreach loop to iterate through your character array
+            foreach(char item in arr)
+            {
+                try
+                {
+                    str = item.ToString();
+                    var number = int.Parse(str);
+                    numbers.Add(number);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Unable to Parse '{item}'");
+                }
+            }
+            // Now create a try catch
             
-                // Now create a try catch
-                
                 
                     // Inside your try block
                         // set your string variable to each array element in your char[] to .ToString()
@@ -39,10 +67,10 @@ namespace ExceptionHandlingExercise
                 
             
 
-            //foreach (var num in numbers)
-            //{
-            //    Console.WriteLine(num);
-            //}
+            foreach (var num in numbers)
+            {
+              Console.WriteLine(num);
+            }
         }
     }
 }
